a1bcf77b9802e2ceb21c26b2f370e07e6ad16e37
Lyn Dee <lyn@Lyns-MacBook-Pro.local>
2016-06-29 11:57:03 -0400

    adding mongoose

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 var express = require('express');
+var mongoose = require('mongoose')
 var app = express();
 var port = process.env.PORT || 3000;
+var MONGODBURI = "mongodb://heroku_pkxb3vz8:9d5sc5jb4aeumftlfdkq3s92tg@ds023654.mlab.com:23654/heroku_pkxb3vz8" || "mongodb://localhost:27017/herokutest"
 
+var Person = mongoose.model('Person', {
+	name: String
+})
 
 app.get("/", function (req, res){
+	Person.create({name: "Lyn"}, function(){
 	res.send("Hi")
+
+	})
+	
+})
+
+mongoose.connect(MONGODBURI)
+
+mongoose.connection.once("open", function(){
+	console.log('mongodb');
 })
 
 app.listen(port, function() {
